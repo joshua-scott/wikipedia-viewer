0848a965f230e7efce27e3f56515662aee8a64bd
Joshua Scott <jscott313@gmail.com>
2017-09-12 18:15:59 +0300

    Switched from promises to async/await

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,16 @@
 const form = document.querySelector('form');
 
-// function executeQuery(e) {
-//   e.preventDefault();
-
-//   const query = form.search.value;
-//   const cors = 'https://cors-anywhere.herokuapp.com/';
-//   const endpoint = `${cors}https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}`;
-//   fetch(endpoint)
-//     .then(blob => blob.json())
-//     .then(data => {
-//       data.query.search.forEach(result => {
-//         console.log(result.title);
-//       })
-//     });
-// }
-
-function executeQuery(e) {
+async function executeQuery(e) {
   e.preventDefault();
 
   const query = form.search.value;
   const cors = 'https://cors-anywhere.herokuapp.com/';
   const endpoint = `${cors}https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&namespace=0&search=${query}`;
-  fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => {
-      console.log('Search term = ', data[0]);
-      console.log(data[1][1]);
-      console.log(data[2][1]);
-      console.log(data[3][1]);
-    });
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  console.dir(data);
 }
 
 form.addEventListener('submit', executeQuery);
