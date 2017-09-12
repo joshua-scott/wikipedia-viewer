ab284d937549bed43b9069758a77b49d05714eeb
Joshua Scott <jscott313@gmail.com>
2017-09-12 21:44:30 +0300

    Changed query method, returned data is better

--- src/js/script.js
+++ src/js/script.js
@@ -4,40 +4,42 @@ const output = document.querySelector('.output');
 async function executeQuery(e) {
   e.preventDefault();
 
-  // Build the URL to make the query
-  const query = form.search.value;
-  const cors = 'https://cors-anywhere.herokuapp.com/';
-  const endpoint = `${cors}https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&namespace=0&search=${query}`;
+  const queryText = form.search.value;
+  const queryOptions = {
+    "action": "query",
+    "format": "json",
+    "formatversion": "2",
+    "prop": "extracts",
+    "exlimit": "max",
+    "explaintext": "1",
+    "exsentences": "1",
+    "exintro": "1",
+    "generator": "search",
+    "gsrnamespace": "0",
+    "gsrlimit": "10",
+    "gsrsearch": `${queryText}`
+  };
 
-  // Get the data, then process it into a usable array of objects.
+  // Build up the query URL using the options above
+  const cors = 'https://cors-anywhere.herokuapp.com/';
+  let endpoint = `${cors}https://en.wikipedia.org/w/api.php?`;
+  Object.keys(queryOptions).forEach(key => endpoint += `&${key}=${queryOptions[key]}`);
+
+  // Get the data, then process it into a nice usable array of objects.
   const response = await fetch(endpoint);
   const json = await response.json();
-  const resultsArr = parseData(json);
+  const results = json.query.pages;
 
-  // Process further until it can be added to the DOM
-  // const domElements = processResults(results);
-  console.dir(resultsArr);
-  processResults(resultsArr);
+  addToDOM(results);
 }
 
-function parseData(json) {
-  const results = [];
-  for (let i = 0; i < json[1].length; i++) {
-    const currentResult = {};
-    currentResult.title = json[1][i];
-    currentResult.body = json[2][i];
-    currentResult.url = json[3][i];
-    results.push(currentResult);
-  }
-  return results;
-}
+function addToDOM(results) {
+  const url = 'https://en.wikipedia.org/?curid=';
 
-function processResults(resultsArr) {
-  const toAppend = resultsArr.reduce((a, b) => {
-    return a + `<a href="${b.url}" target="_blank">${b.title}</a><p>${b.body}</p>`;
+  const toAppend = results.reduce((a, b) => {
+    return a + `<div class="result"><h2><a href="${url + b.pageid}" target="_blank">${b.title}</a></h2><p>${b.extract}</p></div>`;
   }, '');
 
-  console.log(toAppend);
   output.innerHTML = toAppend;
 }
 
