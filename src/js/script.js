6a0a3a2ceade5ebe19a06955bfa8da1e3ee72285
Joshua Scott <jscott313@gmail.com>
2017-09-12 17:53:21 +0300

    Switched to opensearch api as it's more suited for this project

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,33 @@
 const form = document.querySelector('form');
 
+// function executeQuery(e) {
+//   e.preventDefault();
+
+//   const query = form.search.value;
+//   const cors = 'https://cors-anywhere.herokuapp.com/';
+//   const endpoint = `${cors}https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}`;
+//   fetch(endpoint)
+//     .then(blob => blob.json())
+//     .then(data => {
+//       data.query.search.forEach(result => {
+//         console.log(result.title);
+//       })
+//     });
+// }
+
 function executeQuery(e) {
   e.preventDefault();
 
   const query = form.search.value;
   const cors = 'https://cors-anywhere.herokuapp.com/';
-  const endpoint = `${cors}https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}`;
+  const endpoint = `${cors}https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&namespace=0&search=${query}`;
   fetch(endpoint)
     .then(blob => blob.json())
     .then(data => {
-      data.query.search.forEach(result => {
-        console.log(result.title);
-      })
+      console.log('Search term = ', data[0]);
+      console.log(data[1][1]);
+      console.log(data[2][1]);
+      console.log(data[3][1]);
     });
 }
 
